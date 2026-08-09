0330b5722ba78b97b28a03cd12981e5347df3204
chintey <kimteyley@gmail.com>
2026-08-10 02:03:55 +0700

    Move directly toward mouse target

--- neko/Store.swift
+++ neko/Store.swift
@@ -67,36 +67,42 @@ final class Store: ObservableObject {
             }
         case .northWest:
             anim = [.movingNorthWest1, .movingNorthWest2]
-            nekoLoc = move(-1, 1)
         case .north:
             anim = [.movingNorth1, .movingNorth2]
-            nekoLoc = move(0, 1)
         case .northEast:
             anim = [.movingNorthEast1, .movingNorthEast2]
-            nekoLoc = move(1, 1)
         case .east:
             anim = [.movingEast1, .movingEast2]
-            nekoLoc = move(1, 0)
         case .southEast:
             anim = [.movingSouthEast1, .movingSouthEast2]
-            nekoLoc = move(1, -1)
         case .south:
             anim = [.movingSouth1, .movingSouth2]
-            nekoLoc = move(0, -1)
         case .southWest:
             anim = [.movingSouthWest1, .movingSouthWest2]
-            nekoLoc = move(-1, -1)
         case .west:
             anim = [.movingWest1, .movingWest2]
-            nekoLoc = move(-1, 0)
+        }
+
+        if direction != .none {
+            nekoLoc = moveToward(newMouseLoc)
         }
 
         mouseLoc = newMouseLoc
         return nekoLoc
     }
 
-    private func move(_ xSteps: CGFloat, _ ySteps: CGFloat) -> NSPoint {
-        return NSPoint(x: nekoLoc.x + CGFloat(step) * xSteps, y: nekoLoc.y + CGFloat(step) * ySteps)
+    private func moveToward(_ target: NSPoint) -> NSPoint {
+        let xDistance = target.x - nekoLoc.x
+        let yDistance = target.y - nekoLoc.y
+        let distance = sqrt(xDistance * xDistance + yDistance * yDistance)
+        let travel = min(step, distance)
+
+        guard distance > 0 else { return nekoLoc }
+
+        return NSPoint(
+            x: nekoLoc.x + xDistance / distance * travel,
+            y: nekoLoc.y + yDistance / distance * travel
+        )
     }
 }
 
@@ -106,13 +112,16 @@ private func nextDirection(_ mouseLoc: NSPoint, _ nekoLoc: NSPoint) -> Direction
     let d = delta(nekoLoc, mouseLoc)
     let horizontal = abs(d.x)
     let vertical = abs(d.y)
+    let distance = sqrt(d.x * d.x + d.y * d.y)
     let diagonalThreshold: CGFloat = 0.4142
 
-    if horizontal < 1 && vertical < 1 {
+    if distance < 1 {
         return .none
     }
 
-    if horizontal >= 1 && vertical >= horizontal * diagonalThreshold && horizontal >= vertical * diagonalThreshold {
+    if horizontal > 0 && vertical > 0 &&
+        vertical >= horizontal * diagonalThreshold &&
+        horizontal >= vertical * diagonalThreshold {
         if d.x >= 0 {
             return d.y >= 0 ? .southWest : .northWest
         }
